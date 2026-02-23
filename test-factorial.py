194e37dd4008e788ec7ecb1570f60874fcd47b3d
mafarl <108734570+mafarl@users.noreply.github.com>
2026-02-23 11:51:00 +0000

    Update test-factorial.py

--- test-factorial.py
+++ test-factorial.py
@@ -10,16 +10,16 @@ class Testing(unittest.TestCase) :
 # ##########################################################
     def test1(self) :
         trials = {
-            "test1a" : (0 ,1) ,
-            "test1b" : (1 ,1) ,
-            "test1c" : (4 ,24) ,
-            "test1d" : (6 ,720) ,
-            "test1e" : (9 ,362880)
+            "test1a" : (0,1) ,
+            "test1b" : (1,1) ,
+            "test1c" : (4,24) ,
+            "test1d" : (6,720) ,
+            "test1e" : (9,362880)
         }
-        for name , (argument , result ) in trials.items() :
-            with self.subTest( name = name ) :
+        for name, (argument,result) in trials.items() :
+            with self.subTest(name = name) :
                 hdrs = {"Content-Type":"application/json"}
-                js = {"argument": argument }
-                rsp = mock.post("/factorial", headers = hdrs , json = js )
-                self.assertEqual(rsp.status_code ,200)
+                js = {"argument": argument}
+                rsp = mock.post("/factorial",headers = hdrs, json = js)
+                self.assertEqual(rsp.status_code, 200)
                 self.assertEqual(result, rsp.json["result"])
